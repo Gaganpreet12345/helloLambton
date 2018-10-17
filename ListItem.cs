dfb3106c3ac8890a654040adb8745cfe4cedcb02
Gaganpreet Singh <Gaganpreet.Singh@Tor.Loc>
2018-10-17 18:32:36 -0400

    I made list adapter

---
+++ ListItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App3
+{
+    class ListItem
+    {
+
+       public int Id { get; set; }
+       public string Title { get; set; }
+       public string Subtitle { get; set; }
+       public string Distance { get; set; }
+       public string Image { get; set; }
+
+
+    }
+}
